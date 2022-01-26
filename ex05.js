c699c922359a1546447dd36f23f8e43ee0b7330f
diego <diegopgcds10@gmail.com>
2022-01-26 17:09:35 -0300

    Exercicios express router

---
+++ ex05.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const server = express()
+const router = require('./ex05_routes')
+
+server.use('/api', router)
+
+server.listen(4000, () => 
+    console.log('Executando...')
+)
